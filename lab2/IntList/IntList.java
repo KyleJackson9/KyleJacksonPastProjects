2cf49b81c13d8ed5b4ecd4e07a4b758b84d5cb62
KyleJackson9 <kylej.jackson@yahoo.com>
2015-01-29 22:38:00 -0800

    finished intlist

--- lab2/IntList/IntList.java
+++ lab2/IntList/IntList.java
@@ -63,14 +63,32 @@ public class IntList {
 
   public static IntList dcatenate(IntList A, IntList B) {
     //TODO:  fill in method
-    return null;
+    IntList C = A;
+    while(C.tail != null){
+      C = C.tail;
+    }
+    C.tail = B;
+    return A;
   }
 
   /** Returns a list consisting of the elements of A followed by the
    ** elements of B.  May NOT modify items of A.  Use 'new'. */
   public static IntList catenate(IntList A, IntList B) {
     //TODO:  fill in method
-    return null;
+    //if (A == null){
+     // return null;
+   // }
+    IntList res = new IntList(A.head, null);
+    IntList ptr = res;
+    A = A.tail;
+    while (A != null) {
+      ptr.tail = new IntList(A.head, null);
+      A = A.tail;
+      ptr = ptr.tail;
+    }
+    ptr.tail = B;
+    return res;
+
   }
 
 
