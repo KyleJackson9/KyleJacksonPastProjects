5a9f29cf3816ea5f87b68ccff686971a5f51e75d
KyleJackson9 <kylej.jackson@yahoo.com>
2015-02-11 19:52:57 -0800

    piece less public

--- proj0/Piece.java
+++ proj0/Piece.java
@@ -1,11 +1,11 @@
 
 
 public class Piece {
-	public boolean isFire;
-	public Board b;
+	private boolean isFire;
+	private Board b;
 	public int x;
 	public int y;
-	public String type;
+	private String type;
 	public boolean king;
 
 
@@ -39,35 +39,6 @@ public class Piece {
 
 	public boolean isKing(){
 		return king;
-		// if (isFire() && x==7 && y ==7) {
-		// 	king = true;
-		// 	return true;
-		// }
-		// else if (isFire() && x==5 && y ==7) {
-		// 	type = "king";
-		// 	return true;
-		// }
-		// else if (isFire() && x==3 && y ==7) {
-		// 	return true;
-		// }
-		// else if (isFire() && x==1 && y ==7) {
-		// 	return true;
-		// }
-		// else if (isFire() != true && x==0 && y ==0) {
-		// 	return true;
-		// }
-		// else if (isFire() != true && x==2 && y ==0) {
-		// 	return true;
-		// }
-		// else if (isFire() != true && x==4 && y ==0) {
-		// 	return true;
-		// }
-		// else if (isFire() != true && x==6 && y ==0) {
-		// 	return true;
-		// }
-		// else {
-		// 	return false;
-		// }
 	}
 
 	public  boolean isBomb(){
@@ -89,24 +60,25 @@ public class Piece {
 	}
 
 	public void move(int xx, int yy){
-		x = xx; //needs to check for kings & capturing, call upon the board
+		x = xx; 
 		y = yy;
 	}
 
 	public boolean hasCaptured(){
-		if (b.removed != null){
-			if (Math.abs(b.removed.x - x) == 2){
-				if (Math.abs(b.removed.y -y)==2) {
-					return true;
-				}
-			}
-		}
-		return false;
+		// if (b.removed != null){
+		// 	if (Math.abs(b.removed.x - x) == 2){
+		// 		if (Math.abs(b.removed.y -y)==2) {
+		// 			return true;
+		// 		}
+		// 	}
+		// }
+		// return false;
+		return b.captured;
 	}
 
 	public void doneCapturing(){
 		while (hasCaptured()){
-			hasCaptured();
+			b.captured = false;
 		}
 
 	}
