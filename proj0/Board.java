6e39cd4e6fe2296c2cd59cfaf231865556159130
KyleJackson9 <kylej.jackson@yahoo.com>
2015-02-13 12:54:00 -0800

    king maybe

--- proj0/Board.java
+++ proj0/Board.java
@@ -279,6 +279,9 @@ public class Board {
 				return false;
 			}
 		} else if (x>=0 && y>=0 && x<8 && y<8){
+			if (prev != null && prev == select){
+				return false;
+			}
 			if (prev !=null && !prev.hasCaptured()){
 			if (turn % 2 == 0 && select.isFire() ){
 				return true;
@@ -287,7 +290,7 @@ public class Board {
 			}else {
 				return false;
 			}
-		} else {//(prev == null) {
+		} else {
 			if (turn % 2 == 0 && select.isFire() ){
 				return true;
 			} else if (turn % 2 == 1 && !select.isFire()){
