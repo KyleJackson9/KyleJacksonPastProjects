769f5f0c8e843d60dc1ed431ba096538cf3e946d
KyleJackson9 <kylej.jackson@yahoo.com>
2015-03-11 14:59:17 -0700

    checkpoint

--- proj1/ngordnet/TimeSeries.java
+++ proj1/ngordnet/TimeSeries.java
@@ -54,6 +54,8 @@ public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
                         (this.get(year).doubleValue() / (ts.get(year).doubleValue())));
             } else if (ts.containsKey(year)) {
                 divMap.put(year, 0.0);
+            } else if (this.containsKey(year)) {
+                divMap.put(year, this.get(year).doubleValue());
             } else if (!ts.containsKey(year)) {
                 throw new IllegalArgumentException();
             }
