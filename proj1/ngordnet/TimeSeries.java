3ea0b1c65e7bdbea981f1dbdb97f3a860f418bac
KyleJackson9 <kylej.jackson@yahoo.com>
2015-03-10 21:42:55 -0700

    less tabs

--- proj1/ngordnet/TimeSeries.java
+++ proj1/ngordnet/TimeSeries.java
@@ -6,87 +6,87 @@ import java.util.Collection;
 import java.util.Set;
 
 public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
-    private int start;
-    private int end;
-    private TimeSeries<T> copy;
-    private int go;
-    private int no;
+private int start;
+private int end;
+private TimeSeries<T> copy;
+private int go;
+private int no;
 
-    public TimeSeries() {
-        super();
-        start = 0;
-        end = 0;
-        copy = this;
+public TimeSeries() {
+    super();
+    start = 0;
+    end = 0;
+    copy = this;
+}
+
+public TimeSeries(TimeSeries<T> ts, int startYear, int endYear) {
+    super(ts);
+    copy = new TimeSeries<T>();
+    start = startYear;
+    end = endYear;
+    for (int i = start; i <= end; i++) {
+        copy.put(i, ts.get(i));
     }
+}
 
-    public TimeSeries(TimeSeries<T> ts, int startYear, int endYear) {
-        super(ts);
-        copy = new TimeSeries<T>();
-        start = startYear;
-        end = endYear;
-        for (int i = start; i <= end; i++) {
-            copy.put(i, ts.get(i));
+public TimeSeries(TimeSeries<T> ts) {
+    super(ts);
+    copy = ts;
+}
+
+public TimeSeries<Double> dividedBy(TimeSeries<? extends Number> ts) {
+    TimeSeries<Double> divMap = new TimeSeries<Double>();
+
+    for (int i : this.keySet()) {
+        if (!ts.containsKey(i)) {
+            throw new IllegalArgumentException();
+        }
+        if (this.containsKey(i) && ts.containsKey(i)) {
+            divMap.put(i,
+                    (this.get(i).doubleValue() / (ts.get(i).doubleValue())));
+        } else if (this.containsKey(i)) {
+            divMap.put(i, this.get(i).doubleValue());
+        } else if (ts.containsKey(i)) {
+            divMap.put(i, ts.get(i).doubleValue());
         }
     }
+    return divMap;
+}
 
-    public TimeSeries(TimeSeries<T> ts) {
-        super(ts);
-        copy = ts;
-    }
-
-    public TimeSeries<Double> dividedBy(TimeSeries<? extends Number> ts) {
-        TimeSeries<Double> divMap = new TimeSeries<Double>();
-
-        for (int i : this.keySet()) {
-            if (!ts.containsKey(i)) {
-                throw new IllegalArgumentException();
-            }
-            if (this.containsKey(i) && ts.containsKey(i)) {
-                divMap.put(i,
-                        (this.get(i).doubleValue() / (ts.get(i).doubleValue())));
-            } else if (this.containsKey(i)) {
-                divMap.put(i, this.get(i).doubleValue());
-            } else if (ts.containsKey(i)) {
-                divMap.put(i, ts.get(i).doubleValue());
-            }
-        }
-        return divMap;
-    }
-
-    public TimeSeries<Double> plus(TimeSeries<? extends Number> ts) {
-        TimeSeries<Double> pMap = new TimeSeries<Double>();
-        if (this.isEmpty() && !ts.isEmpty()) {
-            for (int i : ts.keySet()) {
-                pMap.put(i, ts.get(i).doubleValue());
-            }
-            return pMap;
-        } else if (!this.isEmpty() && ts.isEmpty()) {
-            for (int i : this.keySet()) {
-                pMap.put(i, this.get(i).doubleValue());
-            }
-            return pMap;
-        }
+public TimeSeries<Double> plus(TimeSeries<? extends Number> ts) {
+    TimeSeries<Double> pMap = new TimeSeries<Double>();
+    if (this.isEmpty() && !ts.isEmpty()) {
         for (int i : ts.keySet()) {
-            if (this.containsKey(i) && ts.containsKey(i)) {
-                pMap.put(i,
-                        (this.get(i).doubleValue() + (ts.get(i).doubleValue())));
-            } else if (this.containsKey(i)) {
-                pMap.put(i, this.get(i).doubleValue());
-            } else if (ts.containsKey(i)) {
-                pMap.put(i, ts.get(i).doubleValue());
-            }
+            pMap.put(i, ts.get(i).doubleValue());
+        }
+        return pMap;
+    } else if (!this.isEmpty() && ts.isEmpty()) {
+        for (int i : this.keySet()) {
+            pMap.put(i, this.get(i).doubleValue());
         }
         return pMap;
     }
-
-    public Collection<Number> years() {
-        Set<Integer> year = this.keySet();
-        ArrayList<Number> x = new ArrayList<Number>();
-        x.addAll(year);
-        return x;
+    for (int i : ts.keySet()) {
+        if (this.containsKey(i) && ts.containsKey(i)) {
+            pMap.put(i,
+                    (this.get(i).doubleValue() + (ts.get(i).doubleValue())));
+        } else if (this.containsKey(i)) {
+            pMap.put(i, this.get(i).doubleValue());
+        } else if (ts.containsKey(i)) {
+            pMap.put(i, ts.get(i).doubleValue());
+        }
     }
+    return pMap;
+}
 
-    public Collection<Number> data() {
-        return (Collection<Number>) this.values();
-    }
+public Collection<Number> years() {
+    Set<Integer> year = this.keySet();
+    ArrayList<Number> x = new ArrayList<Number>();
+    x.addAll(year);
+    return x;
+}
+
+public Collection<Number> data() {
+    return (Collection<Number>) this.values();
+}
 }
--- proj1/ngordnet/YearlyRecord.java
+++ proj1/ngordnet/YearlyRecord.java
@@ -9,142 +9,142 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class YearlyRecord {
-	/** Creates a new empty YearlyRecord. */
-	private TreeMap<String, Integer> map;
-	private TreeMap<Integer, ArrayList<String>> opposite;
-	private TreeMap<String, Integer> rank;
-	private boolean ranker;
+/** Creates a new empty YearlyRecord. */
+private TreeMap<String, Integer> map;
+private TreeMap<Integer, ArrayList<String>> opposite;
+private TreeMap<String, Integer> rank;
+private boolean ranker;
 
-	public YearlyRecord() {
-		map = new TreeMap<String, Integer>();
-		opposite = new TreeMap<Integer, ArrayList<String>>();
-		rank = new TreeMap<String, Integer>();
-		ranker = true;
+public YearlyRecord() {
+	map = new TreeMap<String, Integer>();
+	opposite = new TreeMap<Integer, ArrayList<String>>();
+	rank = new TreeMap<String, Integer>();
+	ranker = true;
+}
+
+/** Creates a YearlyRecord using the given data. */
+public YearlyRecord(HashMap<String, Integer> otherCountMap) {
+	opposite = new TreeMap<Integer, ArrayList<String>>();
+	map = new TreeMap<String, Integer>();
+	rank = new TreeMap<String, Integer>();
+	HashMap<String, Integer> map1 = otherCountMap;
+	int size = 1;
+	List mapKeys = new ArrayList(map1.keySet());
+	List mapValues = new ArrayList(map1.values());
+
+	for (int i = 0; i < mapValues.size(); i++) {
+		ArrayList<String> copy = new ArrayList<String>();
+		copy.add((String) mapKeys.get(i));
+		map.put((String) mapKeys.get(i), (int) mapValues.get(i));
+		opposite.put((int) mapValues.get(i), copy);
+	}
+	this.rerank();
+
+}
+
+/** Comparator that compares strings based on zCount. */
+private class ZComparator implements Comparator<String> {
+	public int compare(String x, String y) {
+		return count(x) - count(y);
+	}
+}
+
+/** Returns the number of times WORD appeared in this year. */
+public int count(String word) {
+	if (map.containsKey(word)) {
+		return map.get(word);
+	}
+	return 0;
+}
+
+private void rerank() {
+	rank = new TreeMap<String, Integer>();
+	String[] words = new String[map.size()];
+	int cnt = 0;
+	for (String word : map.keySet()) {
+		words[cnt] = word;
+		cnt += 1;
 	}
 
-	/** Creates a YearlyRecord using the given data. */
-	public YearlyRecord(HashMap<String, Integer> otherCountMap) {
-		opposite = new TreeMap<Integer, ArrayList<String>>();
-		map = new TreeMap<String, Integer>();
-		rank = new TreeMap<String, Integer>();
-		HashMap<String, Integer> map1 = otherCountMap;
-		int size = 1;
-		List mapKeys = new ArrayList(map1.keySet());
-		List mapValues = new ArrayList(map1.values());
+	// Sort words by order of number of s
+	Arrays.sort(words, new ZComparator());
+	int last = map.get(words[0]);
+	rank.put(words[0], 0);
+	for (int i = 1; i < words.length; i += 1) {
+		int currentZCount = map.get(words[i]);
+		int numWordsLessZs;
 
-		for (int i = 0; i < mapValues.size(); i++) {
-			ArrayList<String> copy = new ArrayList<String>();
-			copy.add((String) mapKeys.get(i));
-			map.put((String) mapKeys.get(i), (int) mapValues.get(i));
-			opposite.put((int) mapValues.get(i), copy);
-		}
-		this.rerank();
-
-	}
-
-	/** Comparator that compares strings based on zCount. */
-	private class ZComparator implements Comparator<String> {
-		public int compare(String x, String y) {
-			return count(x) - count(y);
-		}
-	}
-
-	/** Returns the number of times WORD appeared in this year. */
-	public int count(String word) {
-		if (map.containsKey(word)) {
-			return map.get(word);
-		}
-		return 0;
-	}
-
-	private void rerank() {
-		rank = new TreeMap<String, Integer>();
-		String[] words = new String[map.size()];
-		int cnt = 0;
-		for (String word : map.keySet()) {
-			words[cnt] = word;
-			cnt += 1;
-		}
-
-		// Sort words by order of number of s
-		Arrays.sort(words, new ZComparator());
-		int last = map.get(words[0]);
-		rank.put(words[0], 0);
-		for (int i = 1; i < words.length; i += 1) {
-			int currentZCount = map.get(words[i]);
-			int numWordsLessZs;
-
-			if (currentZCount > last) {
-				numWordsLessZs = i;
-			} else {
-				numWordsLessZs = rank.get(words[i - 1]);
-			}
-
-			rank.put(words[i], numWordsLessZs);
-			last = currentZCount;
-		}
-		for (int i = 0; i < words.length; i += 1) {
-			rank.put(words[i], map.size() - rank.get(words[i]));
-		}
-		ranker = false;
-	}
-
-	/** Records that WORD occurred COUNT times in this year. */
-	public void put(String word, int count) {
-		ranker = true;
-		if (map.containsKey(word)) {
-			for (int i : opposite.keySet()) {
-				if (opposite.get(i).indexOf(word) != -1) {
-					opposite.get(i).remove(word);
-				}
-			}
-		}
-		map.put(word, count);
-
-		if (opposite.containsKey(count)) {
-			opposite.get(count).add(word);
+		if (currentZCount > last) {
+			numWordsLessZs = i;
 		} else {
-			ArrayList<String> copy = new ArrayList<String>();
-			copy.add(word);
-			opposite.put(count, copy);
+			numWordsLessZs = rank.get(words[i - 1]);
 		}
 
+		rank.put(words[i], numWordsLessZs);
+		last = currentZCount;
 	}
-
-	public int size() {
-		return map.size();
+	for (int i = 0; i < words.length; i += 1) {
+		rank.put(words[i], map.size() - rank.get(words[i]));
 	}
+	ranker = false;
+}
 
-	/** Returns all words in ascending order of count. */
-	public Collection<String> words() {
-		ArrayList<String> mapster = new ArrayList<String>();
+/** Records that WORD occurred COUNT times in this year. */
+public void put(String word, int count) {
+	ranker = true;
+	if (map.containsKey(word)) {
 		for (int i : opposite.keySet()) {
-			ArrayList<String> copy = opposite.get(i);
-			for (int j = 0; j < copy.size(); j++) {
-				mapster.add(copy.get(j));
+			if (opposite.get(i).indexOf(word) != -1) {
+				opposite.get(i).remove(word);
 			}
 		}
-		return mapster;
+	}
+	map.put(word, count);
+
+	if (opposite.containsKey(count)) {
+		opposite.get(count).add(word);
+	} else {
+		ArrayList<String> copy = new ArrayList<String>();
+		copy.add(word);
+		opposite.put(count, copy);
 	}
 
-	/** Returns all counts in ascending order of count. */
-	public Collection<Number> counts() {
-		List mapValues = new ArrayList(opposite.keySet());
-		return mapValues;
-	}
+}
 
-	/**
-	 * Returns rank of WORD. Most common word is rank 1. If two words have the
-	 * same rank, break ties arbitrarily. No two words should have the same
-	 * rank.
-	 */
-	public int rank(String word) {
-		if (!map.containsKey(word)) {
-			return -1;
+public int size() {
+	return map.size();
+}
+
+/** Returns all words in ascending order of count. */
+public Collection<String> words() {
+	ArrayList<String> mapster = new ArrayList<String>();
+	for (int i : opposite.keySet()) {
+		ArrayList<String> copy = opposite.get(i);
+		for (int j = 0; j < copy.size(); j++) {
+			mapster.add(copy.get(j));
 		}
-		if (ranker) {
-			rerank();
-		}
-		return rank.get(word);
 	}
+	return mapster;
+}
+
+/** Returns all counts in ascending order of count. */
+public Collection<Number> counts() {
+	List mapValues = new ArrayList(opposite.keySet());
+	return mapValues;
+}
+
+/**
+ * Returns rank of WORD. Most common word is rank 1. If two words have the
+ * same rank, break ties arbitrarily. No two words should have the same
+ * rank.
+ */
+public int rank(String word) {
+	if (!map.containsKey(word)) {
+		return -1;
+	}
+	if (ranker) {
+		rerank();
+	}
+	return rank.get(word);
+}
 }
