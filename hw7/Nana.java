af2a15c2ca817988f63db1c0751523d77df00ff1
KyleJackson9 <kylej.jackson@yahoo.com>
2015-03-28 14:39:46 -0700

    testing

--- hw7/Nana.java
+++ hw7/Nana.java
@@ -81,12 +81,12 @@ public class Nana {
 
     // Is hashCodeA() a perfect hash for Nana? Return "yes" or "no"
     public static final String isPerfectHashA() {
-        return "yes"; // REPLACE THIS LINE WITH YOUR ANSWER 
+        return "no"; // REPLACE THIS LINE WITH YOUR ANSWER 
     }
 
     // Is hashCodeB() a perfect hash for Nana? Return "yes" or "no"
     public static final String isPerfectHashB() {
-        return "yes"; // REPLACE THIS LINE WITH YOUR ANSWER 
+        return "no"; // REPLACE THIS LINE WITH YOUR ANSWER 
     }
 
     // Is hashCodeC() a perfect hash for Nana? Return "yes" or "no"
@@ -106,7 +106,7 @@ public class Nana {
 
     // Is hashCodeF() a perfect hash for Nana? Return "yes" or "no"
     public static final String isPerfectHashF() {
-        return "yes"; // REPLACE THIS LINE WITH YOUR ANSWER 
+        return "no"; // REPLACE THIS LINE WITH YOUR ANSWER 
     }
 
     /**************************************************************/
@@ -129,7 +129,7 @@ public class Nana {
 
     // Is hashCodeD() a valid hash for Nana? Return "yes" or "no"
     public static final String isValidHashD() {
-        return "yes"; // REPLACE THIS LINE WITH YOUR ANSWER 
+        return "no"; // REPLACE THIS LINE WITH YOUR ANSWER 
     }
 
     // Is hashCodeE() a valid hash for Nana? Return "yes" or "no"
@@ -146,7 +146,7 @@ public class Nana {
     /************************ Question #1.3 ***********************/
 
     public static final String worst2Best() {
-        return "B, C, A, D"; // REPLACE THIS LINE WITH YOUR ANSWER 
+        return "C, A, B, D"; // REPLACE THIS LINE WITH YOUR ANSWER 
     }
 
     /**************************************************************/
