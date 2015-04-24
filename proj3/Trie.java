acdfd2d13b1730fabe83b231645dbca178a20a54
KyleJackson9 <kylej.jackson@yahoo.com>
2015-04-24 00:35:48 -0700

    trieme

--- proj3/Trie.java
+++ proj3/Trie.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 /**
  * Prefix-Trie. Supports linear time find() and insert(). 
  * Should support determining whether a word is a full word in the 
@@ -5,7 +6,13 @@
  * @author Kyle Jackson
  */
 public class Trie {
-
+	    /**
+     * Initializes required data structures from parallel arrays.
+     * @param r  length of links.
+     * @param isWord True if its a full word.
+     * @param links All of the children of that Trie.
+     * @param foundFullWord checks if found a full word.
+     */
 	private static final int r = 255;
 	public boolean isWord;
 	public Trie[] links;
@@ -16,7 +23,11 @@ public class Trie {
 		isWord = false;
 		foundFullWord = false;
 	}
-
+	    /**
+     * Find the weight of a given term. If it is not in the dictionary, return 0.0
+     * @param term
+     * @return true if full word is true and it was a full word. 
+     */
     public boolean find(String s, boolean isFullWord) {
     	int found = find(s);
     	if (foundFullWord) {
@@ -27,7 +38,11 @@ public class Trie {
     	return false;
 
     }
-
+    /**
+     * Find the weight of a given term. If it is not in the dictionary, return 0.0
+     * @param s
+     * @return int of how long the string got to before exiting
+     */
     private int find(String s) {
     	Trie sink = this;
     	char[] charArray = s.toCharArray();
@@ -45,7 +60,10 @@ public class Trie {
     	return k;
 
     }
-
+    /**
+     * Find the weight of a given term. If it is not in the dictionary, return 0.0
+     * @param s a String to be inputted.
+     */
     public void insert(String s) {
     	if (s.equals("")) {
     		throw new IllegalArgumentException();
@@ -63,16 +81,30 @@ public class Trie {
     	}
     	sink.isWord = true;
     }
-    public static void main(String[] args) {
-    Trie t = new Trie();
-    t.insert("hello");
-    t.insert("hey");
-    t.insert("goodbye");
-    System.out.println(t.find("hell", false));
-    System.out.println(t.find("hello", true));
-    System.out.println(t.find("good", false));
-    System.out.println(t.find("bye", false));
-    System.out.println(t.find("heyy", false));
-    System.out.println(t.find("hell", true));   
-}
+
+    public LinkedList<String> print(int size) {
+    	LinkedList rec = new LinkedList();
+    	final int BUFFER = 1024;
+    	char[] buffer = new char[BUFFER];	 
+		doApply(rec,0,buffer,this);
+		return rec;
+
+    }
+
+    private void doApply(LinkedList rec, int index, char buffer[], Trie t) {
+    int i = 0;
+	if (t != null) {
+	    if (t.isWord) {
+			rec.add(new String(buffer,0,index));
+			i++;
+	    }
+	    int k;
+	    for(k=0; k < r; k++){
+		if (t.links[k] != null) {
+		    buffer[index] = (char)(k);
+		    doApply(rec,index+1,buffer,t.links[k]);
+		}
+	    }
+	}	    
+    }
 }
