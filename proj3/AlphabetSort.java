3c2f2690e339e374cbfd4fe3408190256341c0e4
KyleJackson9 <kylej.jackson@yahoo.com>
2015-04-28 21:04:25 -0700

    funky variables

--- proj3/AlphabetSort.java
+++ proj3/AlphabetSort.java
@@ -1,11 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.FileInputStream;
-import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.io.IOException;
 import java.util.Scanner;
 /**
  * Alphabet Sort. Supports weird alphabets getting sorted. 
@@ -17,6 +11,7 @@ public class AlphabetSort {
     private HashMap<Character, Character> convert;
     private HashMap<Character, Character> convertBack;
     private Trie t;
+    private static final int l = 256;
         /**
      * Initializes required data structures for conversion.
      */
@@ -32,45 +27,41 @@ public class AlphabetSort {
     public void sort(Scanner sc) {
         String line;
         int count = 0;
-        //char[] alphabet = x.toCharArray();
-        char[] alphabet = new char[256];
-        for (int i=0; i<256; i++){
-          char temp = (char) i;
-          alphabet[i] = temp;
-        }
-        //String x = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        
+        char[] alphabet = new char[l];
+        for (int i = 0; i < 256; i++){
+            char temp = (char) i;
+            alphabet[i] = temp;
+        }      
         Scanner in = sc;
         if (!in.hasNext()) {
            throw new IllegalArgumentException(); 
         }
         char[] alpha = in.nextLine().toCharArray();
-            for (int j = 0; j < alpha.length; j++) {
-                if (!convert.containsKey(alpha[j])) {
-                    convert.put(alpha[j], alphabet[j]);
-                    convertBack.put(alphabet[j], alpha[j]);
+        for (int j = 0; j < alpha.length; j++) {
+            if (!convert.containsKey(alpha[j])) {
+                convert.put(alpha[j], alphabet[j]);
+                convertBack.put(alphabet[j], alpha[j]);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+        while (in.hasNext()) {
+            line = in.nextLine();
+            count++;
+            char[] charWord = line.toCharArray();
+            String toTrie = "";
+            for (int i = 0; i < charWord.length; i++) {
+                if (i == charWord.length - 1 && convert.containsKey(charWord[i])) {
+                    toTrie += convert.get(charWord[i]);
+                    t.insert(toTrie);
+                } else if (convert.containsKey(charWord[i])) {
+                    toTrie += convert.get(charWord[i]);
                 } else {
-                    throw new IllegalArgumentException();
+                    break;
                 }
             }
-
-            while (in.hasNext()) {
-                line = in.nextLine();
-                count++;
-                char[] charWord = line.toCharArray();
-                String toTrie = "";
-                for (int i = 0; i < charWord.length; i++) {
-                    if (i == charWord.length - 1 && convert.containsKey(charWord[i])) {
-                        toTrie += convert.get(charWord[i]);
-                        t.insert(toTrie);
-                    } else if (convert.containsKey(charWord[i])) {
-                        toTrie += convert.get(charWord[i]);
-                    } else {
-                        break;
-                    }
-                }
-            }
-       
+        }
+   
         LinkedList<String> printed = t.print(count);
         for (int i = 0; i < printed.size(); i++) {
             String toPrint = printed.get(i);
@@ -86,6 +77,7 @@ public class AlphabetSort {
 
         /**
      * Spits out the sorted array.
+     * @param args takes System.in
      */
     public static void main(String[] args) {
         AlphabetSort a = new AlphabetSort();
